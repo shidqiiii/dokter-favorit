f9ff657ff0added9c75563118feb960fbcb6e420
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-07-22 00:47:53 +0700

    finish landing page

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,24 +1,33 @@
-import { Button } from 'react-bootstrap'
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 
 export default function NavigationBar() {
+    const [navbar, setNavbar] = useState(false)
+
+    const changeBackgroundNavbar = () => {
+        if (window.scrollY >= 80) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeBackgroundNavbar)
+
     return (
-        <Navbar bg='light' variant='light' expand="lg" sticky="top" className='py-3' >
+        <Navbar expand="lg" sticky="top" className={navbar ? 'active py-3' : 'py-3'} >
             <Container>
-                <Navbar.Brand href="#" className='fw-bold'>DokterFavorit</Navbar.Brand>
+                <Navbar.Brand href="#" className='fw-bold'>DoctorFav</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#">Home</Nav.Link>
-                        <Nav.Link href="#">Product</Nav.Link>
-                        <Nav.Link href="#">Pricing</Nav.Link>
-                        <Nav.Link href="#">Contact</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button>Login <AiOutlineArrowRight /></Button>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#header">About Us</Nav.Link>
+                        <Nav.Link href="#feature">Features</Nav.Link>
+                        <Nav.Link href="#faq">FAQs</Nav.Link>
+                        <Nav.Link href="#newsletter">Contact</Nav.Link>
+                        <Nav.Link href="/login" className='login px-3'>Login <AiOutlineArrowRight /></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import NavigationBar from '../Components/NavigationBar'
 import { Accordion, Button, Card, Col, Container, Form, Row } from 'react-bootstrap'
-import { AiOutlineRead } from "react-icons/ai";
+import { AiOutlineRead, AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { FaCircle } from "react-icons/fa";
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 export default function LandingPage() {
     const sliderEffect = (type) => {
@@ -27,46 +26,47 @@ export default function LandingPage() {
         <>
             <NavigationBar />
 
-            <Container>
-
-                {/* Header */}
-                <div className="header py-5">
+            {/* Header */}
+            <div id='header' className="py-5">
+                <Container>
                     <Row xs={1} lg={2} className='justify-content-center align-items-center'>
-                        <Col md={5} className='d-flex justify-content-center align-items-center'>
+                        <Col md={6} className='d-flex justify-content-center align-items-center'>
                             <Card>
                                 <Card.Body>
                                     <Card.Title className='fw-bolder fs-1'>Never Hesitate to Consult with Doctor</Card.Title>
                                     <Card.Text className='my-3'>
                                         Assertively parallel task team driven delverables with resource-leveling services. Competently transform technically sound strategic theme areas before competitive data.
                                     </Card.Text>
-                                    <Button variant="primary">Book Now</Button>
+                                    <Button>Request an Appoitments</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={5} className='d-flex justify-content-center align-items-center'>
-                            <Card.Img variant="top" src="./Image/doctor.png" />
+                        <Col md={6} className='card-image d-flex justify-content-center align-items-center'>
+                            <Card.Img variant="top" src="./Image/doctor.png" className='px-5 pt-3' />
                         </Col>
                     </Row>
-                </div>
+                </Container>
+            </div>
 
-                {/* Feature */}
-                <div className="feature mb-5">
+            {/* Feature */}
+            <div id='feature' className="py-5">
+                <Container>
                     <header className='text-center mb-5'>
-                        <Card.Text className='my-1'>Why you should trust us?</Card.Text>
+                        <Card.Text className='mb-1'>Why you should trust us?</Card.Text>
                         <Card.Title className='my-2 fw-bold fs-2'>Get to Know About Us</Card.Title>
                     </header>
                     <main>
                         <Row sm={1} md={3} className="gap-4 d-flex justify-content-center align-items-center">
                             {['24/7 Support', 'Private & Secure', 'Million Customers'].map((item, idx) => (
-                                <Col md={5} lg={3} key={idx}>
+                                <Col md={6} lg={3} key={idx}>
                                     <Card className='text-center d-flex justify-content-center align-items-center p-3'>
                                         <div className='icon-feature'>
-                                            <AiOutlineRead size={60} />
+                                            <AiOutlineRead size={50} className='icon' />
                                         </div>
                                         <Card.Body>
                                             <Card.Title className='fw-bold'>{item}</Card.Title>
                                             <Card.Text>
-                                                Assertively parallel task team driven delverables with resource-leveling services.
+                                                They come to be part medinik proffesional & diverse health care.
                                             </Card.Text>
                                         </Card.Body>
                                     </Card>
@@ -74,28 +74,30 @@ export default function LandingPage() {
                             ))}
                         </Row>
                     </main>
-                </div>
+                </Container>
+            </div>
 
-                {/* Feedback */}
-                <div className="feedback mb-5">
-                    <header className='d-flex justify-content-between'>
+            {/* Feedback */}
+            <div id='feedback' className="py-5">
+                <Container>
+                    <header className='d-flex justify-content-between align-items-center'>
                         <Card.Title className='mb-4 fw-bold fs-2'>Start Your <br /> Consultation With Doctor</Card.Title>
-                        <div>
-                            <MdChevronLeft size={40} className="slider-icon left" onClick={() => sliderEffect('slideLeft')} />
-                            <MdChevronRight size={40} className="slider-icon right" onClick={() => sliderEffect('slideRight')} />
+                        <div className='arrow d-flex gap-2'>
+                            <AiOutlineArrowLeft size={35} className="icon" onClick={() => sliderEffect('slideLeft')} />
+                            <AiOutlineArrowRight size={35} className="icon" onClick={() => sliderEffect('slideRight')} />
                         </div>
                     </header>
                     <div className="feedback-card d-flex" id='slider'>
-                        {Array.from({ length: 4 }).map((_, idx) => (
-                            <Card className='p-2' key={idx}>
+                        {Array.from({ length: 8 }).map((_, idx) => (
+                            <Card className='p-2 mx-2' key={idx}>
                                 <Card.Body>
                                     <Card.Text>
                                         Slate helps you see how many more days you need to work to reach your financial goal.
                                     </Card.Text>
                                     <div className='card-profile d-flex gap-3 align-items-center'>
-                                        <Card.Img variant="top" src="./Image/doctor.png" />
+                                        <Card.Img variant="top" src="./Image/doctor.png" className='pt-2' />
                                         <div>
-                                            <Card.Text className='fw-bold m-0'>Mark Ottonomi</Card.Text>
+                                            <Card.Title className='fw-bold m-0 fs-6'>Mark Ottonomi</Card.Title>
                                             <Card.Text>20 y.o <FaCircle size={5} /> Programmer</Card.Text>
                                         </div>
                                     </div>
@@ -103,42 +105,48 @@ export default function LandingPage() {
                             </Card>
                         ))}
                     </div>
-                </div>
+                </Container>
+            </div>
 
-                {/* FAQ */}
-                <div className="faq mb-5">
+            {/* FAQ */}
+            <div id='faq' className="mb-5">
+                <Container>
                     <header className='text-center mb-5'>
                         <Card.Title className='my-2 fw-bold fs-2'>FAQ</Card.Title>
+                        <Card.Text className='mb-1'>What questions are popularly asked?</Card.Text>
                     </header>
                     <Row xs={1} md={2} className="g-4">
-                        {Array.from({ length: 4 }).map((_, idx) => (
+                        {Array.from({ length: 8 }).map((_, idx) => (
                             <Col>
                                 <Accordion>
                                     <Accordion.Item eventKey="0">
-                                        <Accordion.Header>Question Item</Accordion.Header>
+                                        <Accordion.Header>
+                                            <div>
+                                                <Card.Title>Lorem ipsum dolor</Card.Title>
+                                                <Card.Text>Lorem ipsum dolor sit amet</Card.Text>
+                                            </div>                                        </Accordion.Header>
                                         <Accordion.Body>
                                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                            aliquip ex ea commodo consequat.
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                         </Accordion.Body>
                                     </Accordion.Item>
                                 </Accordion>
                             </Col>
                         ))}
                     </Row>
-                </div>
-            </Container >
+                </Container>
+            </div>
+
 
             {/* Newsletter */}
-            <div className="newsletter mb-5 py-3">
-                <Container className='px-5'>
+            <div id='newsletter' className="py-5">
+                <Container >
                     <Row xs={1} lg={2} className='justify-content-center align-items-center'>
                         <Col>
                             <Card>
                                 <Card.Body>
-                                    <Card.Title className='fw-bolder'>Get In Touch</Card.Title>
-                                    <Card.Text className='my-3'>
+                                    <Card.Title className='fw-bolder fs-3'>Get In Touch</Card.Title>
+                                    <Card.Text >
                                         The gradual accumulation of information about atomic and small-scale behavior during the first quarter of the 20th
                                     </Card.Text>
                                 </Card.Body>
@@ -153,6 +161,18 @@ export default function LandingPage() {
                     </Row>
                 </Container>
             </div>
+
+            {/* Footer */}
+            <footer>
+                <ul class="nav justify-content-center border-bottom py-3 mb-3 gap-3">
+                    <li class="nav-item">Home</li>
+                    <li class="nav-item">Featured</li>
+                    <li class="nav-item">About</li>
+                    <li class="nav-item">FAQs</li>
+                    <li class="nav-item">Contact</li>
+                </ul>
+                <Card.Text class="text-center">© 2022 DoctorFav, Inc</Card.Text>
+            </footer>
         </>
     )
 }
