54e4c86fc228bc1660c4fcb3b874ea479dfad869
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-08-11 13:13:29 +0700

    Update DashboardPage.js

--- src/Pages/Auth/DashboardPage.js
+++ src/Pages/Auth/DashboardPage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Card, Col, Container, Row } from 'react-bootstrap'
 import Template from '../../Components/Dashboard Page/Template'
-import { BsFillPersonFill } from "react-icons/bs";
 import { useNavigate } from 'react-router-dom';
+import { RiHistoryFill, RiHospitalLine, RiPencilLine } from 'react-icons/ri';
 
 
 export default function DashboardPage() {
@@ -21,19 +21,39 @@ export default function DashboardPage() {
                         <Card.Text>Hello, Welcome back to DoctorFav App.</Card.Text>
 
                         <Row sm={1} md={3} className="g-4 mb-3">
-                            {["Appointment", "Doctor", "History"].map((item, idx) => (
-                                <Col key={idx}>
-                                    <Card onClick={() => navigateToPage(item)} style={{ cursor: "pointer" }}>
-                                        <Card.Body className='d-flex align-items-center gap-3'>
-                                            <BsFillPersonFill size={50} />
-                                            <div>
-                                                <Card.Text className='my-0'>See {item}</Card.Text>
-                                                <Card.Title>{item}</Card.Title>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            ))}
+                            <Col>
+                                <Card onClick={() => navigateToPage("appointment")} style={{ cursor: "pointer" }}>
+                                    <Card.Body className='d-flex align-items-center gap-3'>
+                                        <RiPencilLine size={50} />
+                                        <div>
+                                            <Card.Text className='my-0'>Make an appointment</Card.Text>
+                                            <Card.Title>Appointment</Card.Title>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card onClick={() => navigateToPage("doctor")} style={{ cursor: "pointer" }}>
+                                    <Card.Body className='d-flex align-items-center gap-3'>
+                                        <RiHospitalLine size={50} />
+                                        <div>
+                                            <Card.Text className='my-0'>See Doctor List</Card.Text>
+                                            <Card.Title>Doctor List</Card.Title>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card onClick={() => navigateToPage("appointment")} style={{ cursor: "pointer" }}>
+                                    <Card.Body className='d-flex align-items-center gap-3'>
+                                        <RiHistoryFill size={50} />
+                                        <div>
+                                            <Card.Text className='my-0'>See History List</Card.Text>
+                                            <Card.Title>History List</Card.Title>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         </Row>
                     </Card.Body>
                 </Card>
